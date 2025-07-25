adae23e647d86b6c1dc0c3cf4f32da0118dcd9d3
Alec Kirkley <alec.w.kirkley@gmail.com>
2025-07-25 19:09:29 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -7,10 +7,10 @@ export default function Page() {
     return (
         <section>
             <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-                HKU Complex Networks Laboratory
+                HKU Complex Networks Lab
             </h1>
             <p className="mb-8">
-                The Complex Networks Lab at the University of Hong Kong investigates the structure, dynamics, and functions of complex systems through the lens of network science. Our interdisciplinary research spans theoretical foundations of network science to applications in transportation, social systems, and biological networks.
+                The HKU Complex Networks Lab is a research group at the University of Hong Kong focused on developing tools for understanding complex systems through the lens of network science. Our work is highly interdisciplinary in nature and draws on ideas from a range of disciplines including statistical physics, information theory, Bayesian inference, scientific computing, and machine learning. 
             </p>
             
             <h2 className="text-xl font-semibold mb-8 tracking-tighter">Research Areas</h2>
@@ -19,7 +19,7 @@ export default function Page() {
             <ResearchArea 
                 title="Developing statistically principled unsupervised learning methods for noisy network data"
                 description="Networks present new challenges for inference and learning due to their relational, hierarchical, and high (usually infinite!) dimensional nature. There are also many ways in which uncertainty can be introduced into network datasets: Networks may be noisy due to statistical or systematic measurement errors; Networks measured in cross-sectional or longitudinal studies may be subject to errors across different experimental environments; Samples of networks or their partitions from a Bayesian posterior distribution are subject to the intrinsic uncertainties in their corresponding statistical model. In this line of research I aim to develop principled, flexible, and interpretable methods to model network data in a variety of forms (temporal/multilayer networks, hypergraphs, etc), which do not overfit or rely on the ad hoc heuristics commonly used in modern machine learning methodology. The methods I develop typically require the formulation of new learning objectives as well as novel sampling and optimization techniques to reveal the patterns of interest and distinguish statistically meaningful structure from noise."
-                image="/images/network-theory.png"
+                image="/images/area1.png"
                 papers={[
                     {
                         title: "Fast nonparametric inference of network backbones for weighted graph sparsification",
@@ -41,7 +41,7 @@ export default function Page() {
             <ResearchArea 
                 title="Improving the efficiency and interpretability of network model fitting and evaluation"
                 description="The discrete, high-dimensional nature of networks also introduces new challenges for model optimization and evaluation, since many objectives of interest are combinatorial in nature, depend on large-scale network properties such as long loops or paths, and produce unwieldy outputs in the form of sets or partitions. As a result, many existing methods for network inference and model evaluation suffer from issues of scalability, interpretability, and subtle but important biases that can compromise the quality of the results in practical applications. In this line of research I aim to develop improved computational methods to enable the scaling of inference methods to large networks as well as robust, interpretable comparisons of network structures based on fundamental principles."
-                image="/images/transport-network.jpg"
+                image="/images/area2.png"
                 papers={[
                     {
                         title: "Belief propagation for networks with loops",
@@ -70,7 +70,7 @@ export default function Page() {
             <ResearchArea 
                 title="Modelling urban mobility networks and their impact on city prosperity and resilience"
                 description="Urban mobility infrastructure and dynamics play a critical role in determining the social, economic, and physical well-being of cities and their residents. Structural networks such as street and other transportation networks facilitate the distribution of resources as well as the accessibility of jobs, amenities, and services. Meanwhile, networks of human mobility dynamics determine the mixing and contact patterns of urban residents, which impacts the spread of ideas and infectious diseases as well as the vibrancy, safety, and congestion of urban spaces."
-                image="/images/social-network.jpg"
+                image="/images/area3.png"
                 papers={[
                     {
                         title: "From the betweenness centrality in street networks to structural invariants in random planar graphs",
@@ -99,7 +99,7 @@ export default function Page() {
             <ResearchArea 
                 title="Characterizing heterogeneity and correlations in urban spatial data with network methods"
                 description="There is an abundance of spatial data available to help understand the structure and function of urban regions, from census data providing demographic information for urban populations, to the precise locations of points of interest such as services or attractions, to satellite imagery capturing large scale land usage patterns. Due to high levels of heterogeneity in population density and the strong dependence of statistical analyses on spatial scale, topologically motivated methods based on spatial network analysis provide a robust lens through which to study urban spatial data."
-                image="/images/social-network.jpg"
+                image="/images/area4.png"
                 papers={[
                     {
                         title: "Spatial regionalization based on optimal information compression",
