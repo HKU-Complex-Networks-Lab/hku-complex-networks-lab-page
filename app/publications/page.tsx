f56d606de986583f190ac39132aacebc73e8bdc7
Alec Kirkley <alec.w.kirkley@gmail.com>
2025-07-26 11:53:20 +0800

    Update page.tsx

--- app/publications/page.tsx
+++ app/publications/page.tsx
@@ -41,7 +41,7 @@ export default function PublicationsPage() {
     
     return (
         <section>
-            <h1 className="font-semibold text-2xl mb-8 tracking-tighter">Publications</h1>
+            <h1 className="font-semibold text-2xl mb-8 tracking-tighter">Publications from junior members of the lab</h1>
             
             <div className="mt-6">
                 {publications.map((pub, i) => (
